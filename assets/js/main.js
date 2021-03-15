f6c11c236a4f3cfa114e7aca9d688633121e84ba
Leonardo <leonardo.bongianni@gmail.com>
2021-03-15 17:51:36 +0100

    Esercizio completo
    esercizio di classe, no controlli

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,16 +2,16 @@
 // Da li parte un timer di 30 secondi.
 // Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
-var i = 0;
 var numeriRandom = [];
+var numeriIndovinati = [];
+var numeriUtente = [];
 
-while (i < 5) {
+while (numeriRandom.length < 5) {
   numeroRandom = randomNumberInRange(1, 50);
   if (numeriRandom.includes(numeroRandom)) {
   } else {
     numeriRandom.push(numeroRandom);
   }
-  i++;
 }
 console.log(numeriRandom);
 
@@ -21,6 +21,23 @@ setTimeout(function(){
   var numeriDom = $("#numeri").text(numeriRandom);
   numeriDom.hide();
 }, 5000);
+
+
+setTimeout(function(){
+  for (var i = 0; i < 5; i++) {
+    var richiestaNum = parseInt(prompt("Che numeri hai visto?"));
+    if (numeriRandom.includes(richiestaNum)) {
+      numeriIndovinati.push(richiestaNum);
+    }
+    $("#risultato").text("Hai indovinato " + numeriIndovinati.length + " numeri! I numeri sono: " + numeriIndovinati);
+}
+}, 6000);
+
+
+console.log(numeriIndovinati);
+
+
+
 
 
 
